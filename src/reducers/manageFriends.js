5d58216bc7d317975e8c69be14c20fe4f3b98d9f
adarvin14 <amdarvin14@gmail.com>
2021-04-02 14:02:17 -0400

    Done

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,20 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {
+    friends: []
+}, action){
+    switch (action.type){
+        case "ADD_FRIEND":
+            return (
+                { ...state,
+                    friends: [...state.friends, action.friend]
+                }
+            )
+        case "REMOVE_FRIEND":
+            let currentFriend = [...state.friends]
+            return({
+                    ...state,
+                    friends: currentFriend.filter(friend => friend.id !== action.id)
+            })
+        default:
+            return state;
+    }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,11 @@
 export function managePresents(state, action){
+    let currentNumOfPres = state.numberOfPresents
+
+    switch (action.type) {
+        
+        case 'INCREASE':
+            return {numberOfPresents: currentNumOfPres + 1}
+        default:
+            return state;
+    }
 }
